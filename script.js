f8bdb9c6b1fdae9808ff9121ca13cd01aeb39ad8
TheStrugglerGuts <astigdolina@gmail.com>
2025-05-03 18:47:32 +0800

    tadeco farms

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Example: you can use this to log clicks on areas
-document.querySelectorAll("area").forEach(area => {
-  area.addEventListener("click", event => {
-    console.log(`Clicked on: ${area.alt}`);
-    // You can also add custom behavior here
+document.querySelectorAll(".map-outline").forEach(area => {
+  area.addEventListener("click", e => {
+    console.log("Clicked region:", area.href);
   });
 });
